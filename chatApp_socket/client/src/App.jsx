aa382411bb289d923a12996ed29e5953c974adba
Nitish Kumar <nitishzira6150@gmail.com>
2024-08-22 20:24:04 +0530

    socket.io

--- chatApp_socket/client/src/App.jsx
+++ chatApp_socket/client/src/App.jsx
@@ -1,35 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useMemo, useState } from "react";
+import { io } from "socket.io-client";
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState([]);
+
+  const [msg, setMsg] = useState("");
+  const [room, setRoom] = useState("");
+  const [socketId, setSocketId] = useState("");
+  const [roomName, setRoomName] = useState("");
+
+  const socket = useMemo(
+    () => io("http://localhost:3000", { withCredentials: true }),
+    []
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", { msg, room });
+    setMsg("");
+  };
+
+  const handleJoinRoom = (e) => {
+    e.preventDefault();
+    socket.emit("join-room", roomName);
+    setRoomName("");
+  };
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setSocketId(socket.id);
+      console.log("CONNECTED, ID: ", socket.id);
+    });
+
+    socket.on("welcome", (data) => {
+      console.log(data);
+    });
+
+    socket.on("receive-message", (data) => {
+      console.log("message received => ", data);
+      setMessages((cur) => [data, ...cur]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container maxWidth="sm">
+      <Typography variant="h3" component="div" gutterBottom>
+        {socketId}
+      </Typography>
+
+      <form onSubmit={handleJoinRoom}>
+        <TextField
+          value={roomName}
+          onChange={(e) => setRoomName(e.target.value)}
+          id="outlined-basic"
+          label="Room Name"
+          variant="outlined"
+        />
+
+        <Button type="submit" variant="contained" color="primary">
+          Join Room
+        </Button>
+      </form>
+
+      <form onSubmit={handleSubmit}>
+        <TextField
+          value={msg}
+          onChange={(e) => setMsg(e.target.value)}
+          id="outlined-basic"
+          label="message"
+          variant="outlined"
+        />
+        <TextField
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+          id="outlined-basic"
+          label="Room ID"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Send
+        </Button>
+      </form>
+
+      {messages.map((mess, i) => (
+        <Typography key={i} variant="h6" component="div" gutterBottom>
+          {mess}
+        </Typography>
+      ))}
+    </Container>
+  );
 }
 
-export default App
+export default App;
--- chatApp_socket/server/app.js
+++ chatApp_socket/server/app.js
@@ -1,6 +1,88 @@
 import express from "express";
-const app = express();
+import { Server, Socket } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
 const PORT = 3000;
+const app = express();
+const secretKeyJWT = "!bdofbsfosb23ey37!";
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/login", (req, res) => {
+  const token = jwt.sign({ _id: "1508" }, secretKeyJWT);
+
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    })
+    .json({
+      msg: "LOGIN SUCCESS",
+    });
+});
+
+io.use((socket, next) => {
+  cookieParser()(socket.request, socket.request.res, (err) => {
+    if (err) return next(err);
+
+    const token = socket.request.cookies.token;
+    if (!token) return next(new Error("Authentication Error"));
+
+    const decoded = jwt.verify(token, secretKeyJWT);
+    next();
+  });
+});
+
+io.on("connection", (socket) => {
+  console.log("User Connected with id: ", socket.id);
+
+  // socket.emit("welcome", `WELCOME TO THE SERVER ${socket.id}`);
+  // socket.broadcast.emit("welcome", `${socket.id} joined the server`);
+
+  socket.on("message", ({ msg, room }) => {
+    console.log("message from frontend: ", msg);
+
+    // SEND TO EVERYONE
+    // io.emit("receive-message", data);
+
+    //  SEND TO EVERYONE EXCEPT MYSELF
+    // socket.broadcast.emit("receive-message", data);
+
+    // SEND TO SOME SPECIFIC USERS
+    socket.to(room).emit("receive-message", msg);
+  });
+
+  socket.on("join-room", (roomName) => {
+    console.log("Someone Joined the Room: ", roomName);
+    socket.join(roomName);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("USER DISCONNECTED", socket.id);
+  });
+});
+
+server.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
